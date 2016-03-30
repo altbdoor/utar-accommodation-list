e83f0106650364d455f7be85c3c702470fe01d15
altbdoor <lancersupraskyline@gmail.com>
2016-03-30 11:43:00 +0800

    finalize travis

--- build_travis.sh
+++ build_travis.sh
@@ -4,6 +4,7 @@
 set -e
 
 # remake the dir
+echo "Cleaning dir"
 rm -rf ./gh-pages/data
 mkdir ./gh-pages/data
 
@@ -11,14 +12,21 @@ mkdir ./gh-pages/data
 python ./get_data.py
 
 # git init
+echo "Setting up git"
 cd gh-pages
 git init
 
 git config user.name "altbdoor"
 git config user.email "lancersupraskyline@gmail.com"
 
-timestamp=$(date +%Y-%m-%dT%H:%M:%S%z)
+# update index html
+timestamp=$(date '+%d %B %Y')
+sed -i "s/{{ update_date }}/$timestamp/g" gh-pages/index.html
+
+# commit
+timestamp=$(date '+%Y-%m-%dT%H:%M:%S%z')
 git add .
 git commit -m "[Travis] updated data on $timestamp"
 
-# git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+echo "Pushing"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
